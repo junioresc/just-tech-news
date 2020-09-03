21d6aae9b65b015678bc5c1ce2d776ba0627e4ca
Junioresc <junioresc1092@gmail.com>
2020-09-02 22:21:00 -0400

    added sessions to keep track of which users are logged in

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const routes = require(`./controllers`);
 const sequelize = require(`./config/connection`);
 const path = require(`path`);
 const exphbs = require(`express-handlebars`);
+const session = require(`express-session`);
+const SequelizeStore = require(`connect-session-sequelize`)(session.Store);
+
+const sess = {
+    secret: `what a time to be alive`,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 const hbs = exphbs.create({});
@@ -13,6 +25,7 @@ app.set(`view engine`, `handlebars`);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, `public`)));
+app.use(session(sess));
 
 app.use(routes);
 
